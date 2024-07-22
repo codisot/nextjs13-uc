694ea3801be94d112bbcc9710eca346a75fc7adf
codisot <codisot@gmail.com>
2024-07-21 21:11:42 -0300

    webhooks

--- lib/actions/user.acton.ts
+++ lib/actions/user.acton.ts
@@ -2,6 +2,9 @@
 
 import User from '@/database/user.model'
 import { connectToDatabase } from '../mongoose'
+import { CreateUserParams, DeleteUserParams, UpdateUserParams } from './shared.types'
+import { revalidatePath } from 'next/cache'
+import Question from '@/database/question.model'
 
 export async function getUserById (params: any): Promise<any> {
   try {
@@ -12,6 +15,56 @@ export async function getUserById (params: any): Promise<any> {
     const user = await User.findOne({ clerkId: userId })
 
     return user
+  } catch (error) {
+    console.log(error)
+    throw error
+  }
+}
+
+export async function createUser (userData: CreateUserParams): Promise<any> {
+  try {
+    await connectToDatabase()
+
+    const nweUser = await User.create(userData)
+    return nweUser
+  } catch (error) {
+    console.log(error)
+    throw error
+  }
+}
+export async function updateUser (params: UpdateUserParams): Promise<any> {
+  try {
+    await connectToDatabase()
+
+    const { clerkId, updateData, path } = params
+
+    await User.findOneAndUpdate({ clerkId }, updateData, { new: true })
+
+    revalidatePath(path)
+  } catch (error) {
+    console.log(error)
+    throw error
+  }
+}
+
+export async function deleteUser (params: DeleteUserParams): Promise<any> {
+  try {
+    await connectToDatabase()
+
+    const { clerkId } = params
+
+    const user = await User.findOneAndDelete({ clerkId })
+
+    if (!user) {
+      throw new Error('user not found')
+    }
+
+    const userQuestionIds = await Question.find({ author: user._id }).distinct('_id')
+
+    await Question.deleteMany({ author: user._id })
+
+    const deleteUser = await User.findOneAndDelete(user._id)
+    return deleteUser
   } catch (error) {
     console.log(error)
     throw error
